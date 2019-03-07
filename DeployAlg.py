ecbe372bb2d094f47cd0856448163ddfd27064c9
Michael Wang <ics238@nyu.edu>
2019-03-07 12:12:36 -0500

    Refine the simulation

---
+++ DeployAlg.py
@@ -0,0 +1,5 @@
+import config as cfg
+from random import randrange
+
+def DummyAlgorithm(target_function, RLstate):
+    return randrange(10)
---
+++ config.py
@@ -0,0 +1,5 @@
+
+### Setting of the data center
+CPU_MAX = 100
+MEM_MAX = 100
+BW_MAX = 100
