7a9de2194ebf4525f0b1e792d40186d27fb15f6c
Michael Wang <ics238@nyu.edu>
2019-03-08 02:27:42 -0500

    The simulation framework is varified/done. Still need reasonable input and algorithms

--- DeployAlg.py
+++ DeployAlg.py
@@ -3,3 +3,5 @@ from random import randrange
 
 def DummyAlgorithm(target_function, RLstate):
     return randrange(10)
+    
+### TODO: RL algorithm
--- config.py
+++ config.py
@@ -1,4 +1,7 @@
 
+### Setting of the simulation
+SIMU_TIME = 1000
+
 ### Setting of the data center
 CPU_MAX = 100
 MEM_MAX = 100
